a71527002f00d115840da781d6ba94ad0490c887
Amol Gupta <62533644+FighterX777@users.noreply.github.com>
2024-07-25 01:46:59 +0530

    Update neko.py

--- neko.py
+++ neko.py
@@ -5,29 +5,6 @@ import requests # type: ignore
 import random
 import gdown # type: ignore
 
-from flask import Flask, render_template
-
-DEVELOPMENT_ENV = True
-
-app = Flask(__name__)
-
-app_data = {
-    "name": "Peter's Starter Template for a Flask Web App",
-    "description": "A basic Flask app using bootstrap for layout",
-    "author": "Peter Simeth",
-    "html_title": "Peter's Starter Template for a Flask Web App",
-    "project_name": "Starter Template",
-    "keywords": "flask, webapp, template, basic",
-}
-
-
-@app.route("/")
-def index():
-    return render_template("index.html", app_data=app_data)
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0',debug=DEVELOPMENT_ENV)
-
 url = 'https://drive.google.com/u/0/uc?id=1RZa_EFxsk7OmzipsD01CuwWLJvf_oxcE'
 output = 'token.txt'
 gdown.download(url, output, quiet=False)
