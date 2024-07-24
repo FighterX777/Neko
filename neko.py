aea9ff2b98f913c3a3acb52545c663cbf5bea1cc
Amol Gupta <62533644+FighterX777@users.noreply.github.com>
2024-07-25 01:35:57 +0530

    Update neko.py

--- neko.py
+++ neko.py
@@ -25,6 +25,9 @@ app_data = {
 def index():
     return render_template("index.html", app_data=app_data)
 
+if __name__ == "__main__":
+    app.run(debug=DEVELOPMENT_ENV)
+
 url = 'https://drive.google.com/u/0/uc?id=1RZa_EFxsk7OmzipsD01CuwWLJvf_oxcE'
 output = 'token.txt'
 gdown.download(url, output, quiet=False)
@@ -71,6 +74,3 @@ async def on_message(message):
     await bot.process_commands(message)
 
 bot.run(TOKEN)
-
-if __name__ == "__main__":
-    app.run(debug=DEVELOPMENT_ENV)
