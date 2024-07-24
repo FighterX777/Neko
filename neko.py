55d54f9bd76c90c736bca2b6e1ead128ace23d71
Amol Gupta <62533644+FighterX777@users.noreply.github.com>
2024-07-25 01:38:56 +0530

    Update neko.py

--- neko.py
+++ neko.py
@@ -26,7 +26,7 @@ def index():
     return render_template("index.html", app_data=app_data)
 
 if __name__ == "__main__":
-    app.run(debug=DEVELOPMENT_ENV)
+    app.run(host='0.0.0.0',debug=DEVELOPMENT_ENV)
 
 url = 'https://drive.google.com/u/0/uc?id=1RZa_EFxsk7OmzipsD01CuwWLJvf_oxcE'
 output = 'token.txt'
